199ba47caab7700c857176bcec1ab656cb9ea513
Jernej <jernej.koren@gmail.com>
2023-02-23 15:15:42 +0100

    build

--- build/module/index.d.ts
+++ build/module/index.d.ts
@@ -1,6 +1,6 @@
 export default class Notification {
     #private;
-    constructor(onsuccess?: () => void, onerror?: () => void);
+    constructor(onsuccess: (() => void) | undefined, onerror: (() => void) | undefined, path: string);
     private init;
     subscribe<T>(channel: string, event: string, callback: (args: T) => void): void;
     message(channel: string, event: string, content: string): void;
